14a435e3456b727783c72ddf8e4befb2da7820be
H3ndys <120047193+H3ndys@users.noreply.github.com>
2022-12-14 15:18:37 -0300

    Add files via upload

---
+++ hendrix.py
@@ -0,0 +1,6 @@
+PaisesSocialistas=["Franca,Grecia,Finlandia,Estonia,cuba"]
+PaisesCapitalistas=["Usa,canada,russia,mexico,japao"]
+
+[PaisesSocialistas,PaisesCapitalistas]=[PaisesSocialistas,PaisesCapitalistas]
+
+print(PaisesSocialistas,PaisesCapitalistas)
